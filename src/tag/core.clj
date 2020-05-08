a3865cbf2b2b6c8a44533518b6d040761604c242
anatoly <tolitius@gmail.com>
2020-05-08 19:12:48 -0400

    add maven intel

--- src/tag/core.clj
+++ src/tag/core.clj
@@ -1,6 +1,9 @@
 (ns tag.core
   (:require [clojure.string :as s]
-            [clojure.java.shell :as sh]))
+            [clojure.java.shell :as sh]
+            [clojure.java.io :as io]
+            [clojure.data.xml :as dx]
+            [xml-in.core :as xml]))
 
 (defn- fmv
   "apply f to each value v of map m"
@@ -16,13 +19,31 @@
       (println (str "(!) could not run \"" cmd "\" => " rs ", but have no fear: I will continue without it."))
       out)))
 
+(defn pom? [pom]
+  (or (.exists (io/file pom))
+      (println "(!) could not find" pom "maven pom file, will skip maven intel")))
+
+(defn- scoop-maven-intel [xpom]
+  (when (pom? xpom)
+    (let [pom (-> (slurp xpom)
+                  dx/parse-str)
+          in-project (xml/find-first pom [:project])
+          project (fn [k] (-> (xml/find-first in-project [k])
+                              first))]
+      {:group-id    (project :groupId)
+       :artifact-id (project :artifactId)
+       :version     (project :version)
+       :name        (project :name)
+       :description (project :description)
+       :url         (project :url)})))
+
 (defn- scoop-git-intel []
-  (-> {:commit-id (dosh "git rev-parse --short HEAD")
-       :version/tag (dosh "git describe --abbrev=0")
-       :repo-url (dosh "git config --get remote.origin.url")
-       :commit-time (dosh "git log -1 --format=%cd")
+  (-> {:commit-id              (dosh "git rev-parse --short HEAD")
+       :version/tag            (dosh "git describe --abbrev=0")
+       :repo-url               (dosh "git config --get remote.origin.url")
+       :commit-time            (dosh "git log -1 --format=%cd")
        "commit human (or not)" (dosh "git log -1 --pretty=format:'%an'")
-       :commit-message (dosh "git log -1 --pretty=%B")}
+       :commit-message         (dosh "git log -1 --pretty=%B")}
       (fmv #(some-> %
                     (s/replace #"\n" " ")
                     s/trim))))
@@ -31,11 +52,15 @@
   ([app-name]
    (describe app-name {:about (str "..and " app-name " is my name")}))
   ([app-name {:keys [about]}]
-   {:about
-    {:app-name app-name
-     "what do I do?" about}
-    :git (scoop-git-intel)
-    :described-at (java.util.Date.)}))
+   (let [maven (scoop-maven-intel "pom.xml")
+         intel {:about
+                {:app-name app-name
+                 "what do I do?" about}
+                :git (scoop-git-intel)
+                :described-at (java.util.Date.)}]
+     (if maven
+       (assoc intel :maven maven)
+       intel))))
 
 (defn export-intel
   ([intel]
